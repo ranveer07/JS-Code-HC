05b769dc104f222d63dc397a242ad56c8a9a31a5
ranveer07 <70086746+ranveer07@users.noreply.github.com>
2025-12-18 06:19:55 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS CODE as newer version
+
+//alert(3 + 3 ) // we are using nodejs not a browser
+
+console.log("Ranveer"); 
+console.log("Kothavale");
+
+let name = "Ranveer"
+let age = 18 
+let isLoggedIn = false
+let state = null
+
+//number => 2 to power 53
+//big int
+// string 
+// boolean false or true
+// null standalone value
+//undefined 
+//symbol => unique
+
+
+//object 
+console.log(typeof undefined) //UNDEFINED
+console.log(typeof null) //OBJECT
